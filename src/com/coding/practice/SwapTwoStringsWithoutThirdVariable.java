2b801fc3850465754cdc514a43d1a418a1d81b8f
davidroshan <davidroshan2021@gmail.com>
2023-04-06 22:08:58 +0530

    Revert "final code for swapping without third variable"
    This reverts commit da2e36b8d211aa3e5c1ae8629e39cc34b2b2eeb7.

--- src/com/coding/practice/SwapTwoStringsWithoutThirdVariable.java
+++ src/com/coding/practice/SwapTwoStringsWithoutThirdVariable.java
@@ -3,23 +3,8 @@ package com.coding.practice;
 public class SwapTwoStringsWithoutThirdVariable {
 
 	public static void main(String[] args) {
-		String firstString = "Hello";
-		String secondString = "World";
-		
-		System.out.println("Before Swap the values:");
-		System.out.println("firstString: "+firstString);
-		System.out.println("secondString: "+secondString);
-		
-		System.out.println();
-		System.out.println();
-		
-		firstString = firstString+secondString;
-		secondString = firstString.substring(0, secondString.length());
-		firstString = firstString.substring(firstString.length()-secondString.length(),
-				                            firstString.length());
-		System.out.println("After swap the values:");
-		System.out.println("firstString: "+firstString);
-		System.out.println("secondString: "+secondString);
+		// TODO Auto-generated method stub
+       int a =10;
 	}
 
 }
