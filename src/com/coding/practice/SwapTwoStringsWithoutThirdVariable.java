da2e36b8d211aa3e5c1ae8629e39cc34b2b2eeb7
davidroshan <davidroshan2021@gmail.com>
2023-04-06 21:56:12 +0530

    final code for swapping without third variable

--- src/com/coding/practice/SwapTwoStringsWithoutThirdVariable.java
+++ src/com/coding/practice/SwapTwoStringsWithoutThirdVariable.java
@@ -3,8 +3,23 @@ package com.coding.practice;
 public class SwapTwoStringsWithoutThirdVariable {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-       int a =10;
+		String firstString = "Hello";
+		String secondString = "World";
+		
+		System.out.println("Before Swap the values:");
+		System.out.println("firstString: "+firstString);
+		System.out.println("secondString: "+secondString);
+		
+		System.out.println();
+		System.out.println();
+		
+		firstString = firstString+secondString;
+		secondString = firstString.substring(0, secondString.length());
+		firstString = firstString.substring(firstString.length()-secondString.length(),
+				                            firstString.length());
+		System.out.println("After swap the values:");
+		System.out.println("firstString: "+firstString);
+		System.out.println("secondString: "+secondString);
 	}
 
 }
